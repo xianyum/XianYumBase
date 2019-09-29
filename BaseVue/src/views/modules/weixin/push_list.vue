0ee7ec2257003958025959dee8168492881d4766
zhangwei <80616059@qq.com>
2019-09-29 17:12:22 +0800

    增加微信推送中心列表展示

---
+++ BaseVue/src/views/modules/weixin/push_list.vue
@@ -0,0 +1,160 @@
+<template>
+  <div class="mod-user">
+    <el-form :inline="true" :model="dataForm" @keyup.enter.native="getDataList()">
+      <el-form-item>
+        <el-input v-model="dataForm.appName" placeholder="推送应用名" clearable></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="getDataList()">查询</el-button>
+        <el-button type="info" @click="reset()">重置</el-button>
+      </el-form-item>
+    </el-form>
+    <el-table
+      :data="dataList"
+      border
+      v-loading="dataListLoading"
+      @selection-change="selectionChangeHandle"
+      style="width: 100%;">
+      <el-table-column
+        type="selection"
+        header-align="center"
+        align="center"
+        width="50">
+      </el-table-column>
+      <el-table-column
+        label="序号"
+        type="index"
+        width="50">
+      </el-table-column>
+      <el-table-column
+        prop="uid"
+        header-align="center"
+        align="center"
+        label="用户ID">
+      </el-table-column>
+      <el-table-column
+        prop="appKey"
+        header-align="center"
+        align="center"
+        label="应用KEY">
+      </el-table-column>
+      <el-table-column
+        prop="appName"
+        header-align="center"
+        align="center"
+        label="应用名">
+      </el-table-column>
+      <el-table-column
+        prop="source"
+        header-align="center"
+        align="center"
+        label="关注来源">
+      </el-table-column>
+      <el-table-column
+        prop="createTime"
+        header-align="center"
+        align="center"
+        label="关注时间"
+        :formatter="formateCreateTime">
+      </el-table-column>
+    </el-table>
+    <el-pagination
+      @size-change="sizeChangeHandle"
+      @current-change="currentChangeHandle"
+      :current-page="pageIndex"
+      :page-sizes="[10, 20, 50, 100]"
+      :page-size="pageSize"
+      :total="totalPage"
+      layout="total, sizes, prev, pager, next, jumper">
+    </el-pagination>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        dataForm: {
+          appName: ''
+        },
+        dataList: [],
+        pageIndex: 1,
+        pageSize: 10,
+        totalPage: 0,
+        dataListLoading: false,
+        dataListSelections: [],
+        addOrUpdateVisible: false
+      }
+    },
+    activated () {
+      this.getDataList()
+    },
+    methods: {
+      formateCreateTime (row, column) {
+        var objD = row.createTime
+        if (!objD) {
+          return ''
+        }
+        objD = new Date(objD)
+        var str
+        var yy = objD.getYear()
+        if (yy < 1900) yy = yy + 1900
+        var MM = objD.getMonth() + 1
+        if (MM < 10) MM = '0' + MM
+        var dd = objD.getDate()
+        if (dd < 10) dd = '0' + dd
+        var hh = objD.getHours()
+        if (hh < 10) hh = '0' + hh
+        var mm = objD.getMinutes()
+        if (mm < 10) mm = '0' + mm
+        var ss = objD.getSeconds()
+        if (ss < 10) ss = '0' + ss
+        str = yy + '-' + MM + '-' + dd + ' ' + hh + ':' + mm + ':' + ss
+        return (str)
+      },
+      reset () {
+        this.dataForm.title = ''
+        this.pageIndex = 1
+        this.pageSize = 10
+        this.getDataList()
+      },
+      // 获取数据列表
+      getDataList () {
+        this.dataListLoading = true
+        this.$http({
+          url: this.$http.adornUrl('/wxCenter/list'),
+          method: 'post',
+          data: this.$http.adornData({
+            'appName': this.dataForm.appName,
+            'pageNum': this.pageIndex,
+            'pageSize': this.pageSize
+          })
+        }).then(({data}) => {
+          if (data && data.code === 200) {
+            this.dataList = data.data.records
+            this.totalPage = data.data.total
+          } else {
+            this.dataList = []
+            this.totalPage = 0
+          }
+          this.dataListLoading = false
+        })
+      },
+      // 每页数
+      sizeChangeHandle (val) {
+        this.pageSize = val
+        this.pageIndex = 1
+        this.getDataList()
+      },
+      // 当前页
+      currentChangeHandle (val) {
+        this.pageIndex = val
+        this.getDataList()
+      },
+      // 多选
+      selectionChangeHandle (val) {
+        this.dataListSelections = val
+      }
+    }
+  }
+</script>
