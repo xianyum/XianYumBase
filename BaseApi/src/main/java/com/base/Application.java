a88861cf55bef793e9cadc5b40170545e3d0bc7b
zhangwei <80616059@qq.com>
2019-09-30 10:18:23 +0800

    集成swagger2

--- BaseApi/src/main/java/com/base/Application.java
+++ BaseApi/src/main/java/com/base/Application.java
@@ -1,5 +1,6 @@
 package com.base;
 
+import com.spring4all.swagger.EnableSwagger2Doc;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,6 +10,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication(exclude= {DataSourceAutoConfiguration.class}) //去掉springboot 默认的数据源配置
 @MapperScan("com.base.dao")
 @EnableScheduling
+@EnableSwagger2Doc
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
