bc4546d1121bec803ff81806533f5eb809153be9
zhangwei <80616059@qq.com>
2019-12-29 23:05:42 +0800

    日志修改

--- BaseVue/src/views/modules/analysis/xiaodao.vue
+++ BaseVue/src/views/modules/analysis/xiaodao.vue
@@ -37,6 +37,7 @@
         prop="url"
         header-align="center"
         align="center"
+        width="250"
         label="url">
         <template slot-scope="scope">
           <a :href="scope.row.url" target="_blank" class="buttonText">{{scope.row.url}}</a>
