b10b238ff404848228e33971e722202229ead543
zhangwei <80616059@qq.com>
2020-08-17 14:26:52 +0800

    更换微信，qq第三方回调地址，切换为：http://xianyum.cn/base/

--- BaseVue/src/views/common/home.vue
+++ BaseVue/src/views/common/home.vue
@@ -18,42 +18,45 @@
       <div class="rightUpdate">
         <h3>更新日志</h3>
         <ul>
-          <el-collapse v-model="activeName" accordion>
+          <el-collapse v-model="activeName" accordion @change="handleChange">
             <el-collapse-item title="不忘初心，方得始终" name="1">
               <div style="color: green">不乱于心，不困于情。 不畏将来，不念过往。</div>
               <div style="color: blue">深谋若谷，深交若水。深明大义，深悉小节。</div>
               <div style="color: red">善宽以怀，善感以恩。善博以浪，善精以业。</div>
               <div style="color: skyblue">无愧于天，无愧于地。无怍于人，无惧于鬼。</div>
             </el-collapse-item>
-            <el-collapse-item title="v1.9 日志增加删除" name="8">
-              <div>2019-12-29 日志增加删除,异步保存用户操作日志.</div>
+            <el-collapse-item title="v2.1 2020-08-17 切换回调地址" name="19">
+              <div>更换微信，qq第三方回调地址，切换为：http://xianyum.cn/base/</div>
             </el-collapse-item>
-            <el-collapse-item title="v1.8 首页改版" name="8">
-              <div>2019-11-27 首页改版，去除天气插件</div>
+            <el-collapse-item title="v2.0 2019-12-29 日志增加删除" name="18">
+              <div>日志增加删除,异步保存用户操作日志.</div>
             </el-collapse-item>
-            <el-collapse-item title="v1.7 接入QQ第三方登录" name="2">
-              <div>2019-11-24 接入QQ第三方登录</div>
+            <el-collapse-item title="v1.8 2019-11-27 首页改版" name="17">
+              <div>首页改版，去除天气插件</div>
             </el-collapse-item>
-            <el-collapse-item title="v1.6 接入支付宝第三方登录，登录界面更换样式" name="3">
-              <div>2019-11-11 接入支付宝第三方登录，登录界面更换样式</div>
+            <el-collapse-item title="v1.7 2019-11-24 接入QQ第三方登录" name="2">
+              <div>接入QQ第三方登录</div>
             </el-collapse-item>
-            <el-collapse-item title="v1.5 加载速度优化，增加微信推送中心功能" name="4">
-              <div>2019-09-30 网站首页初次加载速度优化，新增微信推送中心，用户可以订阅二维码，来进行实时接收消息</div>
+            <el-collapse-item title="v1.6 2019-11-11 接入支付宝第三方登录，登录界面更换样式" name="3">
+              <div>接入支付宝第三方登录，登录界面更换样式</div>
             </el-collapse-item>
-            <el-collapse-item title="v1.4 增加爬虫监控，界面图标美化" name="5">
-              <div>2019-09-27 集成Gecco爬虫框架，把爬取的数据写到数据库里，并且把每天更新内容推送到微信上，以及界面图标美化</div>
+            <el-collapse-item title="v1.5 2019-09-30 加载速度优化，增加微信推送中心功能" name="4">
+              <div>网站首页初次加载速度优化，新增微信推送中心，用户可以订阅二维码，来进行实时接收消息</div>
             </el-collapse-item>
-            <el-collapse-item title="v1.3 注册功能" name="6">
-              <div>2019-06-28 增加用户注册功能，以及用户重复性校验优化</div>
+            <el-collapse-item title="v1.4 2019-09-27 增加爬虫监控，界面图标美化" name="5">
+              <div>集成Gecco爬虫框架，把爬取的数据写到数据库里，并且把每天更新内容推送到微信上，以及界面图标美化</div>
             </el-collapse-item>
-            <el-collapse-item title="v1.2 优化登录模块，增加日志注解" name="7">
-              <div>2019-06-20 优化登录模块，增加日志注解，使用redis存储token，验证码，ip地址等</div>
+            <el-collapse-item title="v1.3 2019-06-28 注册功能" name="6">
+              <div>增加用户注册功能，以及用户重复性校验优化</div>
             </el-collapse-item>
-            <el-collapse-item title="v1.1 版本新增一些常用的工具类,以及集成mybatis-plus多数据源" name="8">
-              <div>2019-06-10 版本新增一些常用的工具类,以及集成mybatis-plus多数据源</div>
+            <el-collapse-item title="v1.2 2019-06-20 优化登录模块，增加日志注解" name="7">
+              <div>优化登录模块，增加日志注解，使用redis存储token，验证码，ip地址等</div>
             </el-collapse-item>
-            <el-collapse-item title="v1.0 开始开发模板" name="9">
-              <div>2019-05-01 v1.0 开始开发模板</div>
+            <el-collapse-item title="v1.1 2019-06-10 版本新增一些常用的工具类,以及集成mybatis-plus多数据源" name="8">
+              <div>版本新增一些常用的工具类,以及集成mybatis-plus多数据源</div>
+            </el-collapse-item>
+            <el-collapse-item title="v1.0 2019-05-01 开始开发模板" name="9">
+              <div>v1.0 开始开发模板</div>
             </el-collapse-item>
           </el-collapse>
         </ul>
@@ -114,6 +117,13 @@
         activeName: '1',
         src: 'http://oss.xianyum.cn/wechat.jpg',
         zhudong: 'http://oss.xianyum.cn/zhudong.jpg'
+      }
+    },
+    methods: {
+      handleChange () {
+        if (this.activeName === '') {
+          this.activeName = '1'
+        }
       }
     }
   }
