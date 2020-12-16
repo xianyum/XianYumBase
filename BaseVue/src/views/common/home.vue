80c7f46f8d05bd10ad4be5c51037cc5236250cd1
zhangwei <80616059@qq.com>
2020-12-16 13:18:34 +0800

    适配最新谷歌浏览器，登录背景图切换为必应每日推荐，首页调整样式

--- BaseVue/src/views/common/home.vue
+++ BaseVue/src/views/common/home.vue
@@ -2,18 +2,35 @@
   <div class="mod-home">
     <div class="main_home">
       <div class="leftProject">
-        <h3>项目介绍</h3>
-        <ul>
-          <li>前后端分离，前端采用vue+element-ui，后端采用springboot主流框架</li>
-          <li>完全响应式布局（支持电脑、平板、手机等所有主流设备）</li>
-          <li>支持多数据源，简单配置即可实现切换。</li>
-          <li>支持按钮及数据权限，可自定义部门数据权限。</li>
-          <li>动态菜单，通过菜单管理统一管理访问路由</li>
-          <li>完善的XSS防范及脚本过滤，彻底杜绝XSS攻击</li>
-          <li>Maven多项目依赖，模块及插件分项目，尽量松耦合，方便模块升级、增减模块。</li>
-          <li>完善的日志记录体系简单注解即可实现</li>
-          <li>支持第三方登录以及管理</li>
-        </ul>
+        <div class="inner-left">
+          <h3>项目介绍</h3>
+          <ul>
+            <li>前后端分离，前端采用vue+element-ui，后端采用springboot主流框架</li>
+            <li>完全响应式布局（支持电脑、平板、手机等所有主流设备）</li>
+            <li>支持多数据源，简单配置即可实现切换。</li>
+            <li>支持按钮及数据权限，可自定义部门数据权限。</li>
+            <li>动态菜单，通过菜单管理统一管理访问路由</li>
+            <li>完善的XSS防范及脚本过滤，彻底杜绝XSS攻击</li>
+            <li>Maven多项目依赖，模块及插件分项目，尽量松耦合，方便模块升级、增减模块。</li>
+            <li>完善的日志记录体系简单注解即可实现</li>
+            <li>支持第三方登录以及管理</li>
+          </ul>
+        </div>
+        <div class="inner-left" style="margin-top:4vh;">
+          <h3 >联系方式</h3>
+          <ul>
+            <img
+              style="margin-left: 20px;height: 160px; box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.4)"
+              :src="src"
+              alt=""
+            >
+            <img
+              style="margin-left: 20px;height: 160px; box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.4)"
+              :src="zhudong"
+              alt=""
+            >
+          </ul>
+        </div>
       </div>
       <div class="rightUpdate">
         <h3>更新日志</h3>
@@ -24,6 +41,9 @@
               <div style="color: blue">深谋若谷，深交若水。深明大义，深悉小节。</div>
               <div style="color: red">善宽以怀，善感以恩。善博以浪，善精以业。</div>
               <div style="color: skyblue">无愧于天，无愧于地。无怍于人，无惧于鬼。</div>
+            </el-collapse-item>
+            <el-collapse-item title="v2.7 2020-12-15 首页样式调整，去除http" name="25">
+              <div>首页样式调整，去除页面中有http请求的地方，适配最新谷歌浏览器</div>
             </el-collapse-item>
             <el-collapse-item title="v2.6 2020-11-21 增加程序订单系统" name="24">
               <div>增加程序订单系统</div>
@@ -77,21 +97,7 @@
         </ul>
       </div>
     </div>
-    <div class="contact">
-      <h3 >联系方式</h3>
-      <ul>
-        <img
-          style="margin-left: 40px;height: 200px;"
-          :src="src"
-          alt=""
-        >
-        <img
-          style="margin-left: 40px;height: 172px;"
-          :src="zhudong"
-          alt=""
-        >
-      </ul>
-    </div>
+
   </div>
 </template>
 <style>
@@ -100,31 +106,30 @@
   }
   .main_home{
     width: 100%;
-    height: 100%;
+    max-height: 85vh;
+    min-width: 1000px;
+    overflow: hidden;
   }
   .main_home .leftProject{
-    position: absolute;
-    margin-left: 20px;
-    width: 52%;
+    margin: 10px 0px 10px 20px;
+    float: left;
+    width: 45%;
+  }
+  .inner-left{
+
+    height: 38vh;
+    padding: 20px;
     box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1)
   }
   .main_home .rightUpdate{
-    position: absolute;
-    margin-left: 54%;
-    width: 720px;
-    float: right;
+    margin: 10px 0px;
+    margin-left: 48%;
+    padding: 20px;
+    max-height: 80vh;
     overflow: auto;
-    margin-right: 20px;
     box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
-    height: 700px;
   }
-  .contact{
-    margin-left: 20px;
-    float:left;
-    width: 54%;
-    margin-top: 281px;
-    box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1)
-  }
+
 </style>
 <script>
   export default {
