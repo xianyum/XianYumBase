e9fb8dbde518d41f37e866d756bb379061b44d19
zhangwei <wei.zhang@onecontract-cloud.com>
2022-11-30 22:24:41 +0800

    测试自动化部署

--- XianYumApi/xianyum-starter/src/main/java/cn/xianyum/starter/XianyumApplication.java
+++ XianYumApi/xianyum-starter/src/main/java/cn/xianyum/starter/XianyumApplication.java
@@ -16,6 +16,7 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 public class XianyumApplication {
 
     public static void main(String[] args) {
+        System.out.println("测试自动化部署");
         SpringApplication.run(XianyumApplication.class, args);
         ProxyServerContainer proxyServerContainer = new ProxyServerContainer();
         proxyServerContainer.start();
