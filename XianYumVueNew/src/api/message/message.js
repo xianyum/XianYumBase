785fd437cfb012536ac0ddab4d08f397efc183a2
zhangwei <80616059@qq.com>
2023-09-21 21:58:09 +0800

    切换前端新项目

--- XianYumVueNew/src/api/message/message.js
+++
@@ -1,9 +0,0 @@
-import request from '@/utils/request'
-
-export function getMessageDetailInfo(data) {
-  return request({
-    url: '/v1/messageMonitor/getById',
-    method: 'post',
-    data: data
-  })
-}
--- XianYumVueNew/src/api/monitor/server.js
+++
@@ -1,9 +0,0 @@
-import request from '@/utils/request'
-
-// 获取服务信息
-export function getServer() {
-  return request({
-    url: 'v1/server/get',
-    method: 'get'
-  })
-}
