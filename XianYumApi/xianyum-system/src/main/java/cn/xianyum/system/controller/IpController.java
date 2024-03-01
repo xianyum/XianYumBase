b518c7105cbafdb8a3a8f44c78c278b52376b49b
zhangwei <wei.zhang@raipiot.com>
2024-03-01 16:35:34 +0800

    测试获取ip归属地

--- XianYumApi/xianyum-system/src/main/java/cn/xianyum/system/controller/IpController.java
+++ XianYumApi/xianyum-system/src/main/java/cn/xianyum/system/controller/IpController.java
@@ -4,6 +4,7 @@ import cn.xianyum.common.annotation.Permission;
 import cn.xianyum.common.utils.Results;
 import cn.xianyum.system.entity.po.IpInfoEntity;
 import cn.xianyum.system.service.IpService;
+import com.alibaba.fastjson2.JSONObject;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 帮助文档：https://gitee.com/lionsoul/ip2region
@@ -26,10 +29,23 @@ public class IpController {
     private IpService ipService;
 
     @GetMapping("/getIpInfo")
-    @ApiOperation(value = "查询IP地理位置", httpMethod = "POST")
+    @ApiOperation(value = "查询IP地理位置")
     @Permission(publicApi = true)
     public Results getIpInfo(@RequestParam(required = false) String ip) {
         IpInfoEntity result = ipService.getIpInfo(ip);
         return Results.success(result);
+    }
+
+    @GetMapping("/getIpInfoRobot")
+    @ApiOperation(value = "查询IP地理位置机器人")
+    @Permission(publicApi = true)
+    public JSONObject getIpInfoRobot(@RequestParam(required = false) String ip) {
+        IpInfoEntity result = ipService.getIpInfo(ip);
+        List<IpInfoEntity> list = new ArrayList<>();
+        list.add(result);
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("err_code",0);
+        jsonObject.put("data_list",list);
+        return jsonObject;
     }
 }
