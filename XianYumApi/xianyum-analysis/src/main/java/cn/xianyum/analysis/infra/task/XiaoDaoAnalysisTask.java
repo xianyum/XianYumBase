70868c5f2bbc77ebe6136228eaafc4d26de4b9e9
zhangwei <wei.zhang@raipiot.com>
2024-03-27 14:41:10 +0800

    增加请求头

--- XianYumApi/xianyum-analysis/src/main/java/cn/xianyum/analysis/infra/task/XiaoDaoAnalysisTask.java
+++ XianYumApi/xianyum-analysis/src/main/java/cn/xianyum/analysis/infra/task/XiaoDaoAnalysisTask.java
@@ -8,10 +8,10 @@ import cn.xianyum.common.handler.IJobHandler;
 import cn.xianyum.common.utils.SchedulerTool;
 import com.xuxueli.crawler.XxlCrawler;
 import com.xuxueli.crawler.parser.PageParser;
+import lombok.extern.slf4j.Slf4j;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.springframework.beans.factory.annotation.Autowired;
-
 import java.util.HashMap;
 import java.util.Map;
 
@@ -20,6 +20,7 @@ import java.util.Map;
  * @date 2019/9/25 15:35
  */
 @JobHandler("xiaoDaoAnalysisTask")
+@Slf4j
 public class XiaoDaoAnalysisTask implements IJobHandler {
 
     @Autowired
@@ -30,8 +31,15 @@ public class XiaoDaoAnalysisTask implements IJobHandler {
     @Override
     public ReturnT execute(Map<String, String> jobMapParams, SchedulerTool tool) throws Exception {
         Map<String, String> headerMap = new HashMap<>();
-        headerMap.put("Cookie","Hm_lvt_d60e542115f2ca02adf147d409bb5f6b=1711291303,1711369895,1711372680,1711462155; Hm_lpvt_d60e542115f2ca02adf147d409bb5f6b=1711467919");
+        headerMap.put("Cookie","Hm_lvt_d60e542115f2ca02adf147d409bb5f6b=1711338582,1711357263,1711520956; Hm_lpvt_d60e542115f2ca02adf147d409bb5f6b=1711521412");
         headerMap.put("User-Agent","Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36");
+        headerMap.put("Sec-Ch-Ua","\"Google Chrome\";v=\"123\", \"Not:A-Brand\";v=\"8\", \"Chromium\";v=\"123\"");
+        headerMap.put("Sec-Ch-Ua-Mobile","?0");
+        headerMap.put("Sec-Ch-Ua-Platform","\"Windows\"");
+        headerMap.put("Sec-Fetch-Dest","document");
+        headerMap.put("Sec-Fetch-Mode","navigate");
+        headerMap.put("Sec-Fetch-Site","none");
+        headerMap.put("Sec-Fetch-User","?1");
         XxlCrawler crawler = new XxlCrawler.Builder()
                 .setUrls(XIAO_DAO_URL)
                 .setAllowSpread(false)
