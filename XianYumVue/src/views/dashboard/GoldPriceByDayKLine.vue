363dd71660223713fc83b280dd04151d33af279e
zhangwei <80616059@qq.com>
2025-02-19 22:42:23 +0800

    增加k线图样式

---
+++ XianYumVue/src/views/dashboard/GoldPriceByDayKLine.vue
@@ -0,0 +1,191 @@
+<template>
+  <div :class="className" :style="{height:height,width:width}" />
+</template>
+
+<script>
+import * as echarts from 'echarts';
+require('echarts/theme/macarons') // echarts theme
+import resize from './mixins/resize'
+
+export default {
+  mixins: [resize],
+  props: {
+    className: {
+      type: String,
+      default: 'chart'
+    },
+    width: {
+      type: String,
+      default: '100%'
+    },
+    height: {
+      type: String,
+      default: '350px'
+    },
+    autoResize: {
+      type: Boolean,
+      default: true
+    },
+    chartData: {
+      type: Array,
+      required: true
+    }
+  },
+  data() {
+    return {
+      chart: null
+    }
+  },
+  watch: {
+    chartData: {
+      deep: true,
+      handler(val) {
+        this.setOptions(val)
+      }
+    }
+  },
+  mounted() {
+    this.$nextTick(() => {
+      this.initChart()
+    })
+  },
+  beforeDestroy() {
+    if (!this.chart) {
+      return
+    }
+    this.chart.dispose()
+    this.chart = null
+  },
+  methods: {
+    calculateMA(dayCount, data) {
+      var result = [];
+      for (var i = 0, len = data.length; i < len; i++) {
+        if (i < dayCount) {
+          result.push('-');
+          continue;
+        }
+        var sum = 0;
+        for (var j = 0; j < dayCount; j++) {
+          sum += +data[i - j][1];
+        }
+        result.push(sum / dayCount);
+      }
+      return result;
+    },
+    initChart() {
+      this.chart = echarts.init(this.$el, 'macarons')
+      this.setOptions(this.chartData)
+    },
+    setOptions(dataArray) {
+      let dates = dataArray.map(item => item[0]);
+      let data = dataArray.map(item => [+item[1], +item[2], +item[5], +item[6]]);
+      this.chart.setOption({
+        legend: {
+          data: ['日K', '开盘价', '最高价', '最低价', '昨收价'],
+          inactiveColor: '#777'
+        },
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            animation: false,
+            type: 'cross',
+            lineStyle: {
+              color: '#376df4',
+              width: 2,
+              opacity: 1
+            }
+          }
+        },
+        xAxis: {
+          type: 'category',
+          data: dates,
+          axisLine: { lineStyle: { color: '#8392A5' } }
+        },
+        yAxis: {
+          scale: true,
+          axisLine: { lineStyle: { color: '#8392A5' } },
+          splitLine: { show: false }
+        },
+        grid: {
+          bottom: 80
+        },
+        dataZoom: [
+          {
+            textStyle: {
+              color: '#8392A5'
+            },
+            handleIcon:
+              'path://M10.7,11.9v-1.3H9.3v1.3c-4.9,0.3-8.8,4.4-8.8,9.4c0,5,3.9,9.1,8.8,9.4v1.3h1.3v-1.3c4.9-0.3,8.8-4.4,8.8-9.4C19.5,16.3,15.6,12.2,10.7,11.9z M13.3,24.4H6.7V23h6.6V24.4z M13.3,19.6H6.7v-1.4h6.6V19.6z',
+            dataBackground: {
+              areaStyle: {
+                color: '#8392A5'
+              },
+              lineStyle: {
+                opacity: 0.8,
+                color: '#8392A5'
+              }
+            },
+            brushSelect: true
+          },
+          {
+            type: 'inside'
+          }
+        ],
+        series: [
+          {
+            type: 'candlestick',
+            name: 'Day',
+            data: data,
+            itemStyle: {
+              color: '#FD1050',
+              color0: '#0CF49B',
+              borderColor: '#FD1050',
+              borderColor0: '#0CF49B'
+            }
+          },
+          {
+            name: '开盘价',
+            type: 'line',
+            data: this.calculateMA(5, data),
+            smooth: true,
+            showSymbol: false,
+            lineStyle: {
+              width: 1
+            }
+          },
+          {
+            name: '最高价',
+            type: 'line',
+            data: this.calculateMA(10, data),
+            smooth: true,
+            showSymbol: false,
+            lineStyle: {
+              width: 1
+            }
+          },
+          {
+            name: '最低价',
+            type: 'line',
+            data: this.calculateMA(20, data),
+            smooth: true,
+            showSymbol: false,
+            lineStyle: {
+              width: 1
+            }
+          },
+          {
+            name: '昨收价',
+            type: 'line',
+            data: this.calculateMA(30, data),
+            smooth: true,
+            showSymbol: false,
+            lineStyle: {
+              width: 1
+            }
+          }
+        ]
+      })
+    }
+  }
+}
+</script>
