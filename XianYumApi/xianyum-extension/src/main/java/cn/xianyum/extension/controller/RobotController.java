d159253acc0eaaa02d2c136f9723b761824f7ed2
xianyum <80616059@qq.com>
2025-11-18 21:53:27 +0800

    机器人请求参数修改

--- XianYumApi/xianyum-extension/src/main/java/cn/xianyum/extension/controller/RobotController.java
+++ XianYumApi/xianyum-extension/src/main/java/cn/xianyum/extension/controller/RobotController.java
@@ -6,8 +6,10 @@ import cn.xianyum.extension.entity.request.RobotRequest;
 import cn.xianyum.extension.entity.request.ServerConfigRequest;
 import cn.xianyum.extension.entity.response.RobotResponse;
 import cn.xianyum.extension.service.RobotService;
+import com.alibaba.fastjson2.JSONObject;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +25,7 @@ import javax.annotation.Resource;
 @RestController
 @RequestMapping("xym-extension/v1/robot")
 @Api(tags = "机器人接口")
+@Slf4j
 public class RobotController {
 
     @Resource
@@ -32,6 +35,7 @@ public class RobotController {
     @PostMapping(value = "/auto-reply")
     @Permission(publicApi = true)
     public RobotResponse autoReply(@RequestBody RobotRequest request) {
-        return robotService.autoReply(request.getRequest().getContent());
+        log.info("机器人请求参数：{}", JSONObject.toJSONString(request));
+        return robotService.autoReply(request.getContent());
     }
 }
