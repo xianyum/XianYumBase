90e1d61225697dd5b5dd0c3afa6889f4b9cf5b4e
xianyum <80616059@qq.com>
2025-12-02 21:12:06 +0800

    修复为0的数据

--- XianYumApi/xianyum-extension/src/main/java/cn/xianyum/extension/service/impl/EvDriveRecordsServiceImpl.java
+++ XianYumApi/xianyum-extension/src/main/java/cn/xianyum/extension/service/impl/EvDriveRecordsServiceImpl.java
@@ -84,7 +84,7 @@ public class EvDriveRecordsServiceImpl implements EvDriveRecordsService {
         this.checkForDuplicateData(request);
         EvDriveRecordsEntity bean = BeanUtils.copy(request, EvDriveRecordsEntity.class);
         BigDecimal electricityPerKm = BigDecimalUtils.divide(bean.getElectricityConsumed(), new BigDecimal(String.valueOf(bean.getDistanceKm())));
-        bean.setElectricityPerKm(electricityPerKm);
+        bean.setElectricityPerKm(Objects.nonNull(electricityPerKm)?electricityPerKm:BigDecimal.ZERO);
         boolean isNormalStatus = this.checkNormalStatus(electricityPerKm);
         bean.setStatus(isNormalStatus? YesOrNoEnum.YES.getStatus() : YesOrNoEnum.NO.getStatus());
         return evDriveRecordsMapper.insert(bean);
