3d8d2fe3e69170c6a3d89cb369afee89f3f3db7c
xianyum <80616059@qq.com>
2026-01-11 01:01:42 +0800

    增加移动端模块

---
+++ XianYumApi/xianyum-mqtt/src/main/java/cn/xianyum/mqtt/controller/TestController.java
@@ -0,0 +1,32 @@
+package cn.xianyum.mqtt.controller;
+
+import cn.xianyum.common.annotation.Permission;
+import cn.xianyum.common.utils.RedisUtils;
+import cn.xianyum.common.utils.Results;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author xianyum
+ * @date 2026/1/11 0:59
+ */
+@RestController
+@RequestMapping("xym-iot/v1/test")
+@Tag(name = "测试")
+@Slf4j
+public class TestController {
+
+    @Resource
+    private RedisUtils redisUtils;
+    @GetMapping("/test")
+    @Permission(publicApi = true)
+    public Results test(@RequestParam Integer day){
+        log.info("day:{}",day);
+        return Results.success(redisUtils.setDay("cn.xianyum.test","1",day));
+    }
+}
