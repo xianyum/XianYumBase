3c6e5376c62dbd5bd07c60ae5fa8fc878fba90ea
xianyum <80616059@qq.com>
2026-01-31 21:07:42 +0800

    验证码增加url

---
+++ XianYumApi/xianyum-common/src/main/java/cn/xianyum/common/constant/CaptchaConstant.java
@@ -0,0 +1,24 @@
+package cn.xianyum.common.constant;
+
+/**
+ * @author xianyum
+ * @date 2026/1/31 21:01
+ */
+public class CaptchaConstant {
+
+    // ====================== 验证码模板文件路径常量 ======================
+    /** 滑块1模板-轮廓图片路径 */
+    public static final String SLIDER_TEMPLATE_1_ACTIVE_PATH = "META-INF/cut-image/template/slider_1/active.png";
+    /** 滑块1模板-凹槽图片路径 */
+    public static final String SLIDER_TEMPLATE_1_FIXED_PATH = "META-INF/cut-image/template/slider_1/fixed.png";
+
+    // ====================== 验证码资源URL常量 ======================
+    /** 验证码默认背景图URL（统一管理，便于后续替换） */
+    public static final String CAPTCHA_DEFAULT_BACKGROUND_URL = "https://picsum.photos/600/360";
+
+    // ====================== 资源类型常量 ======================
+    /** 资源类型-类路径 */
+    public static final String RESOURCE_TYPE_CLASSPATH = "classpath";
+    /** 资源类型-URL */
+    public static final String RESOURCE_TYPE_URL = "url";
+}
--- XianYumApi/xianyum-framework/src/main/java/cn/xianyum/framwork/config/CaptchaResourceConfiguration.java
+++ XianYumApi/xianyum-framework/src/main/java/cn/xianyum/framwork/config/CaptchaResourceConfiguration.java
@@ -1,11 +1,18 @@
 package cn.xianyum.framwork.config;
 
+import cloud.tianai.captcha.application.ImageCaptchaApplication;
+import cloud.tianai.captcha.application.TACBuilder;
 import cloud.tianai.captcha.common.constant.CaptchaTypeConstant;
-import cloud.tianai.captcha.resource.ResourceStore;
-import cloud.tianai.captcha.resource.impl.LocalMemoryResourceStore;
+import cloud.tianai.captcha.resource.common.model.dto.ResourceMap;
+import cloud.tianai.captcha.spring.store.impl.RedisCacheStore;
+import cn.xianyum.common.constant.CaptchaConstant;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import cloud.tianai.captcha.resource.common.model.dto.Resource;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import static cloud.tianai.captcha.generator.impl.StandardSliderImageCaptchaGenerator.TEMPLATE_ACTIVE_IMAGE_NAME;
+import static cloud.tianai.captcha.generator.impl.StandardSliderImageCaptchaGenerator.TEMPLATE_FIXED_IMAGE_NAME;
 
 /**
  * @author xianyum
@@ -14,25 +21,22 @@ import cloud.tianai.captcha.resource.common.model.dto.Resource;
 @Configuration
 public class CaptchaResourceConfiguration {
 
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
     @Bean
-    public ResourceStore resourceStore() {
-        // 使用简单的本地内存存储器，实际项目中可以使用数据库等存储
-        LocalMemoryResourceStore resourceStore = new LocalMemoryResourceStore();
-
-        // 2. 添加自定义背景图片
-        resourceStore.addResource(CaptchaTypeConstant.SLIDER, new Resource("classpath", "bgimages/a.jpg", "default"));
-        resourceStore.addResource(CaptchaTypeConstant.SLIDER, new Resource("classpath", "bgimages/b.jpg", "default"));
-        resourceStore.addResource(CaptchaTypeConstant.SLIDER, new Resource("classpath", "bgimages/c.jpg", "default"));
-        resourceStore.addResource(CaptchaTypeConstant.SLIDER, new Resource("classpath", "bgimages/d.jpg", "default"));
-        resourceStore.addResource(CaptchaTypeConstant.SLIDER, new Resource("classpath", "bgimages/e.jpg", "default"));
-        resourceStore.addResource(CaptchaTypeConstant.SLIDER, new Resource("classpath", "bgimages/g.jpg", "default"));
-        resourceStore.addResource(CaptchaTypeConstant.SLIDER, new Resource("classpath", "bgimages/h.jpg", "default"));
-        resourceStore.addResource(CaptchaTypeConstant.SLIDER, new Resource("classpath", "bgimages/i.jpg", "default"));
-        resourceStore.addResource(CaptchaTypeConstant.SLIDER, new Resource("classpath", "bgimages/j.jpg", "default"));
-        resourceStore.addResource(CaptchaTypeConstant.ROTATE, new Resource("classpath", "bgimages/48.jpg", "default"));
-        resourceStore.addResource(CaptchaTypeConstant.CONCAT, new Resource("classpath", "bgimages/48.jpg", "default"));
-        resourceStore.addResource(CaptchaTypeConstant.WORD_IMAGE_CLICK, new Resource("classpath", "bgimages/c.jpg", "default"));
-
-        return resourceStore;
+    public ImageCaptchaApplication imageCaptchaApplication() {
+        ResourceMap template1 = new ResourceMap(2);
+        // 滑块轮廓图片
+        template1.put(TEMPLATE_ACTIVE_IMAGE_NAME, new Resource(CaptchaConstant.RESOURCE_TYPE_CLASSPATH, CaptchaConstant.SLIDER_TEMPLATE_1_ACTIVE_PATH));
+        // 滑块凹槽图片
+        template1.put(TEMPLATE_FIXED_IMAGE_NAME, new Resource(CaptchaConstant.RESOURCE_TYPE_CLASSPATH, CaptchaConstant.SLIDER_TEMPLATE_1_FIXED_PATH));
+        return TACBuilder.builder()
+                .addTemplate(CaptchaTypeConstant.SLIDER, template1)
+                .addResource(CaptchaTypeConstant.SLIDER, new Resource(CaptchaConstant.RESOURCE_TYPE_URL, CaptchaConstant.CAPTCHA_DEFAULT_BACKGROUND_URL))
+                .addResource(CaptchaTypeConstant.CONCAT, new Resource(CaptchaConstant.RESOURCE_TYPE_URL, CaptchaConstant.CAPTCHA_DEFAULT_BACKGROUND_URL))
+                .addResource(CaptchaTypeConstant.WORD_IMAGE_CLICK, new Resource(CaptchaConstant.RESOURCE_TYPE_URL, CaptchaConstant.CAPTCHA_DEFAULT_BACKGROUND_URL))
+                .setCacheStore(new RedisCacheStore(stringRedisTemplate))
+                .build();
     }
 }
