bb530b95838164449dcb58ffeccba26f2af2cc26
xianyum <80616059@qq.com>
2026-02-13 15:07:41 +0800

    切换OSS

--- XianYumApi/xianyum-system/src/main/java/cn/xianyum/system/service/impl/FileServiceImpl.java
+++ XianYumApi/xianyum-system/src/main/java/cn/xianyum/system/service/impl/FileServiceImpl.java
@@ -50,46 +50,46 @@ public class FileServiceImpl implements FileService {
      */
     @Override
     public FileDetailResponse uploadFile(MultipartFile file) {
-        if(file.isEmpty()){
+        if (file.isEmpty()) {
             throw new SoException("上传的文件为空");
         }
         FileInfo fileInfo = this.fileStorageService.of(file)
                 .setPath(DateUtils.format(new Date(), DateUtils.DATE_PATTERN) + Constants.SLASH)
                 .upload();
-        return this.selectFileById(fileInfo.getId(),true);
+        return this.selectFileById(fileInfo.getId(), true);
     }
 
     /**
      * 查询文件信息
+     *
      * @param fileId
      * @param isCached true 带缓存 false 不带缓存
      * @return
      */
     @Override
-    public FileDetailResponse selectFileById(String fileId,boolean isCached) {
-        if(StringUtil.isEmpty(fileId)){
+    public FileDetailResponse selectFileById(String fileId, boolean isCached) {
+        if (StringUtil.isEmpty(fileId)) {
             return null;
         }
-        String redisKey = String.format(presignedUrlPrefix,fileId);
+        String redisKey = String.format(presignedUrlPrefix, fileId);
 
-        if(isCached && redisUtils.hasKey(redisKey)){
+        if (isCached && redisUtils.hasKey(redisKey)) {
             FileDetailResponse response = JSONObject.parseObject(redisUtils.getString(redisKey), FileDetailResponse.class);
             response.setExpireTime(new Date().getTime() + redisUtils.getExpire(redisKey) * 1000);
             return response;
         }
         FileInfo fileInfo = fileDetailService.selectFileById(fileId);
-        if(Objects.isNull(fileInfo)){
+        if (Objects.isNull(fileInfo)) {
             return null;
         }
-//      String presignedUrl = fileStorageService.generatePresignedUrl(fileInfo, DateUtil.offsetHour(new Date(), 1));
-        FileDetailResponse response = BeanUtil.copyProperties(fileInfo,FileDetailResponse.class);
-//        if(StringUtil.isNotBlank(presignedUrl)){
-        response.setFileUrl(fileInfo.getUrl());
-        response.setExpireTime(new Date().getTime() + 3600*1000L);
-        redisUtils.setMin(redisKey,JSONObject.toJSONString(response),60);
-//        }
+        String presignedUrl = fileStorageService.generatePresignedUrl(fileInfo, DateUtil.offsetHour(new Date(), 1));
+        FileDetailResponse response = BeanUtil.copyProperties(fileInfo, FileDetailResponse.class);
+        if (StringUtil.isNotBlank(presignedUrl)) {
+            response.setFileUrl(presignedUrl);
+            response.setExpireTime(new Date().getTime() + 3600 * 1000L);
+            redisUtils.setMin(redisKey, JSONObject.toJSONString(response), 60);
+        }
         return response;
-
     }
 
     /**
@@ -100,8 +100,8 @@ public class FileServiceImpl implements FileService {
      */
     @Override
     public String presignedUrl(String fileId) {
-        FileDetailResponse response = this.selectFileById(fileId,true);
-        return Objects.nonNull(response)?response.getFileUrl():null;
+        FileDetailResponse response = this.selectFileById(fileId, true);
+        return Objects.nonNull(response) ? response.getFileUrl() : null;
     }
 
 }
