f61daa72cdb053f40fb7792992cab5e55a1a62fc
xianyum <80616059@qq.com>
2026-03-25 22:17:17 +0800

    优化菜单显示问题

--- XianYumApp/uni_modules/otpauth/index.js
+++ XianYumApp/uni_modules/otpauth/index.js
@@ -1,6 +1,8 @@
 
 import * as OTPAuth from './lib/otpauth.esm.min.js';
 
+// 基于：https://github.com/hectorm/otpauth
+// 演示：https://otpauth.molinero.dev/
 // 导出给外部使用
 export default OTPAuth;
 export { OTPAuth };
